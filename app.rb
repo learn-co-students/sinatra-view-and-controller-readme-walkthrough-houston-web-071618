a7a8de2ccaaebbaf57114502f7f11153f6a7eed5
Flatiron School <flatironschool@Flatirons-MacBook-Air-3.local>
2018-08-07 12:01:40 -0500

    Done.

--- app.rb
+++ app.rb
@@ -6,11 +6,16 @@ class App < Sinatra::Base
   end
 
   post '/reverse' do
+    orig_string = params["string"]
+    @rev_string = orig_string.reverse
     erb :reversed
   end
 
   get '/friends' do
     # Write your code here!
+    @friends = ['Emily Wilding Davison', 'Harriet Tubman', 'Joan of Arc', 'Malala Yousafzai', 'Sojourner Truth','Kenny Yang', 'Nancy Do', 'Panda', 'Imaginary 1', 'you, yes you. i see you looking at my code.']
+
+    erb :friends
 
   end
 end
